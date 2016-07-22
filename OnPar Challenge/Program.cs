2c14bc1c0d8426f2be9aff36df00fc40a6470e52
Ben Demko <bendemko7@gmail.com>
2016-07-21 20:47:05 -0400

    setup

--- OnPar Challenge/Program.cs
+++ OnPar Challenge/Program.cs
@@ -10,6 +10,34 @@ namespace OnPar_Challenge
     {
         static void Main(string[] args)
         {
+            string again;
+
+            do
+            {
+                Console.WriteLine("Enter a number to sum.");
+                string number1AsString = Console.ReadLine();
+                int number1 = Convert.ToInt32(number1AsString);
+
+                Console.WriteLine("Enter a second number to sum.");
+                string number2AsString = Console.ReadLine();
+                int number2 = Convert.ToInt32(number1AsString);
+
+            
+                Console.WriteLine(number1 + number2);
+                Console.WriteLine("Do you want to enter another number, y/n?");
+                again = Console.ReadLine();
+
+                //I can't figure out how to sum two numbers and then add additional numbers to that sum
+
+            }
+            
+
+            while (again == "y");
+
+        
         }
+        
+
     }
-}
+
+    }
